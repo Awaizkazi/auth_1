95bd4c3e645c4596785b6e5cdda08e01c36c3a97
Awaizkazi <Owaizkazi789@gmail.com>
2023-06-23 00:04:16 +0530

    c

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,7 +6,28 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade200,
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          // Logo
+
+          // Welcome back you'\ve been missed!
+
+          // Username textField
+
+          // Password textField
+
+          // Forgot password
+
+          // sign In button
+
+          //  or Continue with text
+
+          // google + apple sign In button
+
+          // not a member ? Register now 
+        ],
+      ),
     );
   }
 }
